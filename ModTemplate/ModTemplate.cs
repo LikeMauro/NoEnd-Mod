79631151c80af486525a6a51be6f7036d193b7dc
LikeMauro <104032732+LikeMauro@users.noreply.github.com>
2022-04-21 17:05:58 +0200

    Name Changed

--- ModTemplate/ModTemplate.cs
+++ ModTemplate/ModTemplate.cs
@@ -1,9 +1,10 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
+using UnityEngine.InputSystem;
 
-namespace ModTemplate
+namespace NoEnd
 {
-    public class ModTemplate : ModBehaviour
+    public class NoEnd : ModBehaviour
     {
         private void Awake()
         {
@@ -12,19 +13,10 @@ namespace ModTemplate
             // Use Start() instead.
         }
 
-        private void Start()
+        private void Update()
         {
-            // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(ModTemplate)} is loaded!", MessageType.Success);
+            
 
-            // Example of accessing game code.
-            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
-            {
-                if (loadScene != OWScene.SolarSystem) return;
-                var playerBody = FindObjectOfType<PlayerBody>();
-                ModHelper.Console.WriteLine($"Found player body, and it's called {playerBody.name}!",
-                    MessageType.Success);
-            };
         }
     }
 }
